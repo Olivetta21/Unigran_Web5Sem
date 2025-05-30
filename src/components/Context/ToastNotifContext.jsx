3504617747c96e198f926821def5769d259295bc
Olivetta21 <ivancraftbr@gmail.com>
2025-05-30 09:28:18 -0400

    Atv15 toastnotification

---
+++ src/components/Context/ToastNotifContext.jsx
@@ -0,0 +1,39 @@
+import { useState, createContext, useContext } from "react"; 
+import "./ToastNotif.css";
+
+const toastNotifContext = createContext();
+
+export default function ToastNotifProvider({children}) {
+    const [toasts, setToasts] = useState([]);
+
+    function addToast(mensagem, tipo = "info") {
+        const id = Math.random().toString(36).substring(2, 15);
+        
+        setToasts(prevToasts => [...prevToasts, { mensagem, tipo, id }]);
+
+        setTimeout(() => {
+            removeToast(id);
+        }, 30000);
+    }
+
+    function removeToast(id) {
+        setToasts(prevToasts => prevToasts.filter(toast => toast.id !== id));
+    }
+
+    return (
+        <toastNotifContext.Provider value={{ toasts, addToast, removeToast }}>
+            <div className="toast-container">
+                {toasts.map(toast => (
+                    <div onClick={() => removeToast(toast.id)} key={toast.id} className={`toast toast-${toast.tipo}`}>
+                        <span> {toast.mensagem} </span>
+                    </div>
+                ))}
+            </div>
+            {children}
+        </toastNotifContext.Provider>
+    );
+}
+
+export function useToast() {
+    return useContext(toastNotifContext);
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './components/Auth/AuthContext.jsx'
 import DataProvider from './components/Context/DataContext.jsx'
+import ToastNotifProvider from './components/Context/ToastNotifContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <DataProvider>
-          <App />
-        </DataProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <ToastNotifProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <DataProvider>
+            <App />
+          </DataProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ToastNotifProvider>
   </StrictMode>,
 )
